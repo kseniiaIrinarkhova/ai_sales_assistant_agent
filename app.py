f420b12ec2bab4619c2bb47cab26f5461afed022
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-09 17:53:56 -0800

    added sidebar for LLM settings

--- app.py
+++ app.py
@@ -4,10 +4,32 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.tools import TavilySearchResults
+import requests
 
 # set default model
 if 'default_model' not in st.session_state:
     st.session_state['default_model'] = 'llama3-8b-8192'
+
+# Model and Agent Tools
+llm = ChatGroq(api_key=st.secrets['GROQ_API_KEY'])
+parser = StrOutputParser()
+search = TavilySearchResults(max_results=2)
+
+# function to change default LLM
+def change_model():
+    st.session_state['default_model'] = st.session_state.model
+    print(st.session_state)
+    return
+
+# get list of models
+url = "https://api.groq.com/openai/v1/models"
+
+headers = {
+    "Authorization": f"Bearer {st.secrets['GROQ_API_KEY']}",
+    "Content-Type": "application/json"
+}
+
+models = [model['id'] for model in requests.get(url, headers=headers).json()['data'] if model['context_window'] == 8192 and model['active'] == True] 
 
 # Page Config
 st.set_page_config(page_title="AI Sales Assistant")
@@ -15,6 +37,7 @@ st.set_page_config(page_title="AI Sales Assistant")
 # Sidebar
 st.sidebar.title("AI Sales Assistant settings")
 temperature = st.sidebar.slider('Temperature', min_value=0.0, max_value=1.0, value=0.5, step=0.01, key='temperature')
+st.sidebar.selectbox('Model', models, index=models.index(st.session_state['default_model']), key='model', on_change=change_model)
 
 
 # Main Page
