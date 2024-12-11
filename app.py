9a136808a9761b2024542fb8c81a69119e984f77
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-10 18:40:25 -0800

    improved prompts

--- app.py
+++ app.py
@@ -36,21 +36,24 @@ st.text(f"AI Sales Assistant Powered by Groq. Settings: LLM **{st.session_state.
 st.markdown("### Help sales teams gather insights about their products, competitors, and target customers.")
 
 # LLM settup
-# prompt
-main_prompt = """
+# prompts
+#prompt for sales analysis
+analysis_prompt = """
     You are a helpful AI Sales assistant. Your job is to analyze the provided data to generate insights. Focus on company strategy, competitor analysis, and leadership info and using optional information if provided. 
-    Provide reports based on provided structure:
-
+    Create a report based on provided structure:
+    # Product report
     1. ## Company Strategy: Insights into the company/'s activities and priorities from company_data.
     2. ## Competitor Mentions: Mentions of competitors from competitors_data.
     2.1 ### Competitor Analysis: Insights into competitors from competitors_data.
-    2.2 ### Competitor Comparison: Comparison of the company with competitors from competitors_data.
+    2.2 ### Competitor Comparison: Comparison of the company with competitors from competitors_data using value_proposition if they provided as main advantages.
     3. ## Leadership Information: Relevant leaders and their roles.
     4. ## Product/Strategy Summary: Insights from product_data.
     5. ## Target Market: Proc and cons of launching the product into a target market from target_market.
     3. ##References: Links to articles, press releases, or other sources.
 
-    Create an email template to target_customer from company Leadership based on report. Emphasize the benefits of the product and the company's strengths. Include optional information if provided.
+    # Email template
+    Based on report, create an email template to target_customer. Emphasize the benefits of the product and the company's strengths. Include optional information if provided. 
+    Sing the imail with the name of company CEO if it was mentioned in Leadership Information from report.
 
     Input variables:
     - company_name: {company_name}
@@ -58,47 +61,38 @@ main_prompt = """
     - product_name: {product_name}
     - product_category: {product_category}
     - product_data: {product_data}
+    - value_proposition: {value_proposition}
     - competitors_data: {competitors_data}
     - target_market: {target_market}
     - target_customer: {target_customer}
     - optional: {optional}
 
     """
-# prompt template
-# main_prompt_template = ChatPromptTemplate([('system',main_prompt)])
-main_prompt_template = ChatPromptTemplate.from_template(main_prompt)
 
-
-
+#prompt for product data creation
 product_prompt = """
     You are a helpful AI assistant. 
     Your job is to analyse provided {company_data} and find information about {product_name}. 
-    Include information about {product_category} and {product_description} if provided. 
+    Include information about {product_category} and {value_proposition} if provided. 
     If you can not find information about product, return its name and category.
-"""
+    """
+
+# prompt templates
+
+analysis_prompt_template = ChatPromptTemplate.from_template(analysis_prompt)
 product_prompt_template = PromptTemplate.from_template(template=  product_prompt)
-# product_chain = LLMChain(llm = llm,
-#                          prompt = product_prompt_template,
-#                          output_key = 'product_data')
+
+#chains
 
 product_chain =  product_prompt_template | llm | parser
+analysis_chain = analysis_prompt_template | llm | parser
 
-# analysis_chain = LLMChain(llm = llm,
-#                          prompt = main_prompt_template,
-#                          output_key = 'insights')
-analysis_chain = main_prompt_template | llm | parser
-# chain
-# chain = main_prompt_template | llm | parser
-# full_chain = SequentialChain(
-#     chains = [product_chain, analysis_chain],
-#     input_variables = ['company_name', 'company_data', 'product_name', 'product_category', 'product_description',  'competitors_data', 'target_customer', 'target_market', 'optional'],
-#     output_variables = ['product_data', 'insights']
-#     )
-
+# Main form
 with st.form("product_research", clear_on_submit=True):
+    # form fields
     product_name = st.text_input("Product")
     product_category = st.text_input("Product Category")
-    product_description = st.text_area("Product Description")
+    value_proposition = st.text_area("Value Proposition")
     company_name = st.text_input("Company Name")
     company_website = st.text_input("Company Website")    
     competitors = st.text_area("Competitors")
@@ -106,27 +100,29 @@ with st.form("product_research", clear_on_submit=True):
     target_customer = st.text_input("Target Customer Name")
     optional = st.text_area("Optional")
     submit_button = st.form_submit_button(label='Submit')
+
+    # Output data
     insights = ""
+    product_data = ""
+
+    # Check if the form is submitted
     if submit_button:
         if company_name and product_name:
             with st.spinner("Analyzing the data..."):
                 if company_website:
                     company_data = search.invoke(company_website)
-
                 else:
                     company_data = f"Company: {company_name}"
-                # product_data = f"Product: {product_name}, Category: {product_category}"
-                product_data = product_chain.invoke({'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'product_description':product_description})
+                # getting product data
+                product_data = product_chain.invoke({'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'value_proposition':value_proposition})
                 competitors_data = competitors
-                # target_customer = target_customer
-                # optional = optional
+                # getting insights
+                insights = analysis_chain.invoke({'company_name': company_name, 'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'product_data':product_data, 'value_proposition':value_proposition, 'competitors_data': competitors_data, 'target_customer': target_customer,'target_market': target_market,'optional': optional})
 
-                # insights = chain.invoke({'company_name': company_name, 'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'product_data': product_data, 'competitors_data': competitors_data, 'target_customer': target_customer,'target_market': target_market,'optional': optional})
-                # insights = full_chain({'company_name': company_name, 'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'product_description':product_description, 'competitors_data': competitors_data, 'target_customer': target_customer,'target_market': target_market,'optional': optional})
-                insights = analysis_chain.invoke({'company_name': company_name, 'company_data': company_data, 'product_name': product_name, 'product_category': product_category, 'product_data':product_data, 'competitors_data': competitors_data, 'target_customer': target_customer,'target_market': target_market,'optional': optional})
-
-if insights:
+if product_data:
     st.markdown("### Product Data")
     st.markdown(product_data)
+
+if insights:
     st.markdown("### Product Insights")
     st.markdown(insights)
